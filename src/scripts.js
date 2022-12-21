a6f505fe87ada1899242c6a1cf1b2e34aee53d05
“crlnvls” <cveloso@hotmail.co.uk>
2022-12-21 22:50:16 +0000

    Add dom for buttons

---
+++ src/scripts.js
@@ -0,0 +1,7 @@
+const numberBtn = document.querySelectorAll(".number");
+const operatorBtn = document.querySelectorAll(".operator");
+const equalBtn = document.querySelector(".equal");
+const clearBtn = document.querySelector(".clear");
+const allClearBtn = document.querySelector(".all-clear");
+const previousValue = document.querySelector(".previous");
+const currentValue = document.querySelector(".current");
